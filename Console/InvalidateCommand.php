645a25a042d079ce309b0114de82d7f5e54d3346
Max Chadwick <mpchadwick@gmail.com>
2019-10-07 15:17:51 +0000

    Forward call to Model

--- Console/InvalidateCommand.php
+++ Console/InvalidateCommand.php
@@ -2,12 +2,22 @@
 
 namespace SomethingDigital\InvalidateAdminPasswords\Console;
 
+use SomethingDigital\InvalidateAdminPasswords\Model\Invalidator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class InvalidateCommand extends Command
 {
+    private $invalidator;
+
+    public function __construct(Invalidator $invalidator)
+    {
+        parent::__construct(null);
+
+        $this->invalidator = $invalidator;
+    }
+
     protected function configure()
     {
         $this->setName('sd:invalidate-admin-passwords:invalidate');
@@ -16,6 +26,6 @@ class InvalidateCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        echo 'HELLO' . PHP_EOL;
+        echo $this->invalidator->invalidate();
     }
 }
---
+++ Model/Invalidator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SomethingDigital\InvalidateAdminPasswords\Model;
+
+class Invalidator
+{
+    public function invalidate()
+    {
+        return 'HELLO' . PHP_EOL;
+    }
+}
