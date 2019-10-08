a62c14cfaf0b7262bb2f8485c8857ac76f2b4109
Max Chadwick <mpchadwick@gmail.com>
2019-10-08 18:16:17 +0000

    Add test with send_email off

--- Test/Integration/Model/InvalidatorTest.php
+++ Test/Integration/Model/InvalidatorTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\Mail\Template\TransportBuilderMock;
 use Magento\Framework\App\State;
+use Magento\Framework\Mail\Template\TransportBuilder;
 use Magento\User\Model\ResourceModel\User\CollectionFactory as UserCollectionFactory;
 use SomethingDigital\InvalidateAdminPasswords\Model\Invalidator;
 
@@ -15,12 +16,43 @@ class InvalidatorTest extends TestCase
 
     private $userCollectionFactory;
 
+    private $transportBuilderMock;
+
     protected function setUp()
     {
         parent::setUp();
         $objectManager = Bootstrap::getObjectManager();
-        $this->invalidator = $objectManager->create(Invalidator::class);
         $this->userCollectionFactory = $objectManager->create(UserCollectionFactory::class);
+
+        $this->transportBuilderMock = $this->createMock(TransportBuilder::class);
+
+        $this->transportBuilderMock->method('setTemplateIdentifier')
+            ->willReturn($this->transportBuilderMock);
+
+        $this->transportBuilderMock->method('setTemplateModel')
+            ->willReturn($this->transportBuilderMock);
+
+        $this->transportBuilderMock->method('setTemplateOptions')
+            ->willReturn($this->transportBuilderMock);
+
+        $this->transportBuilderMock->method('setTemplateVars')
+            ->willReturn($this->transportBuilderMock);
+
+        $this->transportBuilderMock->method('setFrom')
+            ->willReturn($this->transportBuilderMock);
+
+        $this->transportBuilderMock->method('addTo')
+            ->willReturn($this->transportBuilderMock);
+
+        $this->transportBuilderMock->method('getTransport')
+            ->willReturn(new \Magento\TestFramework\Mail\TransportInterfaceMock());
+
+        $this->invalidator = $objectManager->create(
+            Invalidator::class,
+            [
+                'transportBuilder' => $this->transportBuilderMock
+            ]
+        );
     }
 
     /**
@@ -28,21 +60,26 @@ class InvalidatorTest extends TestCase
      */
     public function testInvalidate()
     {
-        $transportMock = Bootstrap::getObjectManager()->get(
-            TransportBuilderMock::class
-        );
+        $this->transportBuilderMock->expects($this->atLeastOnce())
+            ->method('setTemplateIdentifier');
+
         $this->invalidator->invalidate();
         $userCollection = $this->userCollectionFactory->create();
         foreach ($userCollection as $user) {
             $this->assertEquals($user->getPassword(), Invalidator::INVALIDATED_PASSWORD_STRING);
         }
+    }
 
-        $message = $transportMock->getSentMessage();
-        $this->assertNotEmpty($message);
-        $this->assertEquals(
-            __('Please reset your admin password'),
-            $message->getSubject()
-        );
+    /**
+     * @magentoDataFixture createAdminUser
+     * @magentoAdminConfigFixture admin/emails/sd_invalidate_admin_passwords_send_email 0
+     */
+    public function testInvalidateWithSendEmailOff()
+    {
+        $this->transportBuilderMock->expects($this->never())
+            ->method('setTemplateIdentifier');
+
+        $this->invalidator->invalidate();
     }
 
     public static function createAdminUser()
